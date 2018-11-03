2834d2f0e19c8a19df6664b87247fddeb8430f32
Christoph Wiekart <cwiekart@xs4all.nl>
2018-11-03 14:23:04 +0100

    first setup game module

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,6 +1,27 @@
 const Game = (_ => {
+  const letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const words = ['apple', 'cat', 'dog', 'ball', 'elephant'];
+  let chosenWord;
+  let guessingWord;
+  let lives;
+  let guesses;
+
+  // cache the DOM
+  const $hangman = document.querySelector('.hangman');
+
+
   const init = _ => {
-    console.log("game activated");
+    // 1 chose a word
+    chosenWord = chooseWord();
+    // 2 build our guessing word to  render
+    guessingWord = Array(chosenWord.length).fill('_');
+    guesses = [];
+    lives = 7
+  }
+
+  const chooseWord = _ => {
+    let randNum = Math.floor(Math.random() * words.length);
+    return words[randNum]
   }
   return {
     init
